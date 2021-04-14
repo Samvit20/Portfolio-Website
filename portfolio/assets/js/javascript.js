edcb0c8f6a55b13ea5baef466aa86f7e1a62ee0c
Samvit.S <samvit999@gmail.com>
2021-04-14 23:52:19 +0530

    Portfolio

---
+++ portfolio/assets/js/javascript.js
@@ -0,0 +1,14 @@
+const icons = document.querySelectorAll('.icon');
+        icons.forEach (icon => {  
+        icon.addEventListener('click', (event) => {
+            var x = document.getElementById("myLinks");
+          if (x.style.display === "block") {
+            x.style.display = "none";
+          } else {
+            x.style.display = "block";
+          }
+          /*icon.classList.toggle("open");*/
+        });
+        });
+        
+        
